cca7e928821846e07ea90f6ff1ca2b9353087384
KaueGuimaraes <76141331+KaueGuimaraes@users.noreply.github.com>
2020-12-17 11:51:13 -0500

    Create ex004.py

---
+++ ex004.py
@@ -0,0 +1,11 @@
+print('========= DESAFIO 004 =========\n')
+
+letra = input('Digite algo: ')
+
+print('\nÉ um número? ', letra.isnumeric())
+print('É alfabético? ', letra.isalpha())
+print('É alfanúmerico? ', letra.isalnum())
+print('Está em maiúsculo? ', letra.isupper())
+print('Está em minúsculo? ), letra.islower())
+
+print('\nFIM!!')
