175a1d26399e65f5002566c5747ae268d4f05397
KaueGuimaraes <76141331+KaueGuimaraes@users.noreply.github.com>
2020-12-17 09:00:45 -0500

    ex001.py

---
+++ ex001.py
@@ -0,0 +1,8 @@
+print('========= DESAFIO 001 =========')
+print()
+
+nome = input('Qual é o seu nome')
+print('Seja bem vindo' + nome + 'este é o meu primeiro script python!')
+
+print()
+print('FIM!!')
