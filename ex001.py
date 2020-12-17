ca25201a57d2948598133587cb5e1af43e19b7fa
KaueGuimaraes <76141331+KaueGuimaraes@users.noreply.github.com>
2020-12-17 10:45:42 -0500

    Update ex001.py

--- ex001.py
+++ ex001.py
@@ -2,7 +2,22 @@ print('========= DESAFIO 001 =========')
 print()
 
 nome = input('Qual é o seu nome')
-print('Seja bem vindo' + nome + 'este é o meu primeiro script python!')
+print('Seja bem vindo' + nome + ' este é o meu primeiro Script Python!')
 
 print()
 print('FIM!!')
+
+###
+
+English Version
+
+print('========= Challenge 001 =========')
+print()
+
+name = input('What is your name?')
+print('Wellcome' + name + ' this is my firt Python Script!')
+
+print()
+print('END!!')
+
+###
