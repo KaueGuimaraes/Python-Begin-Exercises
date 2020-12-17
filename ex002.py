860239980bfbf4abbd08cc0b3c2266f2c10262be
KaueGuimaraes <76141331+KaueGuimaraes@users.noreply.github.com>
2020-12-17 11:20:08 -0500

    Update ex002.py

--- ex002.py
+++ ex002.py
@@ -6,7 +6,7 @@ mes = input('Em que mes você nasceu? ')
 ano = input('Em que ano você nasceu? ')
 
 print()
-print('Você nasceu dia' + dia + ' de ' + mes + ' de ' + ano + ' certo?')
+print('Você nasceu dia ' + dia + ' de ' + mes + ' de ' + ano + ', certo?')
 
 print()
 print('FIM!!')
@@ -19,6 +19,13 @@ print('========= CHALENGE 002 =========')
 print()
 
 day = input('What day were you born? ')
+month = input('What mounth were you born? ')
+year = input('What year were you born? ')
 
+print()
+print('You born in ' + month + ' ' + day + 'th ' + year + ', right?')
+
+print()
+print('END!!')
 
 ###
