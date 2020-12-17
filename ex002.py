315c19dcbaed2f0aafa3d74b68ecb403ee510f34
KaueGuimaraes <76141331+KaueGuimaraes@users.noreply.github.com>
2020-12-17 11:32:44 -0300

    ex002.py

---
+++ ex002.py
@@ -0,0 +1,12 @@
+print('========= DESAFIO 002 =========')
+print()
+
+dia = input('Em que dia você nasceu? ')
+mes = input('Em que mes você nasceu? ')
+ano = input('Em que ano você nasceu? ')
+
+print()
+print('Você nasceu dia' + dia + ' de ' + mes + ' de ' + ano + ' certo?')
+
+print()
+print('FIM!!')
