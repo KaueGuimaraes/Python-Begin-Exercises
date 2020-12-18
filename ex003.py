150591462835cacd1f9008945b1eb642b8aba72d
KaueGuimaraes <76141331+KaueGuimaraes@users.noreply.github.com>
2020-12-17 21:19:55 -0500

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -5,7 +5,7 @@ n1 = int(input('Primeiro número: '))
 n2 = int(input('Segundo número: '))
 
 print()
-print('A soma entre' , n1, 'e', n2, 'é igual a: ', n1 + n2)
+print('A soma entre', n1, 'e', n2, 'é igual a: ', n1 + n2)
 
 print()
 print('FIM!!')
@@ -14,13 +14,10 @@ print('FIM!!')
 
 print('========= CHALLENGE 003 =========')
 print()
-
 n1 = int(input('First number: '))
 n2 = int(input('Second number: '))
-
 print()
 print('The sum between' , n1, 'and', n2, 'is equals: ', n1 + n2)
-
 print()
 print('END!!')
 
