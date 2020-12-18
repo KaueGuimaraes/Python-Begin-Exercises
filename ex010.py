bc751b18d4026634d869868b75dacff94670c894
KaueGuimaraes <76141331+KaueGuimaraes@users.noreply.github.com>
2020-12-18 13:41:57 -0500

    Add files via upload

---
+++ ex010.py
@@ -0,0 +1,33 @@
+print('========= DESAFIO 010 =========\n')
+
+reais = float(input('Digite quantos reais você tem: R$'))
+
+dolar = 3.27
+resultado = reais / dolar
+
+print('\nVocê pode comprar ${:.4} dolares com R${} reais'.format(resultado, reais)) #Na época que eu fiz esse exercício(2018) o dolar valia R$3.27 reais
+
+print('\nFIM!!') #Hoje em dia quando eu estou revisando esse meu exercício antigo(final de 2020) o Dolar está valendo R$5.09 reais
+
+'''
+
+English Version
+
+print('========= CHALLENGE 010 =========\n')
+
+reais = float(input('Type how much reais you have: R$')) #Reais = Brazilian money
+
+dolar = 3.27
+result = reais / dolar
+
+print('\nYou can buy ${:.4} dolars with R${} reais'.format(result, reais)) #When I did this exercise(2018) one dolar cost R$3.27 reais
+
+print('\nEND!!') #Nowadays when i am reviewing this old exercise(final de 2020) one dolar está costs R$5.09 reais
+
+'''
+
+'''
+Desafio: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar.
+
+Challende: Make a program that reads how much reais a people have, and shows how much dolars the user can buy.
+'''
---
+++ ex011.py
@@ -0,0 +1,37 @@
+print('========= DESAFIO 011 =========\n')
+
+l = float(input('Digite a largura de uma parede: '))
+a = float(input('Agora digite a altura dessa mesma parede: '))
+
+q = l * a
+w = q / 2
+
+print('\nA área de {} e {} é {} e precisará de {}L de tinta'.format(l, a, q, w))
+
+print('\nFIM!!')
+
+'''
+
+English Version
+
+print('========= CHALLENGE 011 =========zz\n')
+
+l = int(input('Type a width of a wall: '))
+a = int(input('Now type a height of this wall: '))
+
+q = l * a
+w = q / 2
+
+print('\nThe area of {} and {} is {} and de need {}L of ink'.format(l, a, q, w))
+
+print('\nEND!!')
+
+'''
+
+'''
+Desafio: Faça um programa que leia a largura e a altura de uma parede em metros, calcule sua área e a quantidade
+         de tinta necessária para pinta-la, sabendo que cada litro de tinta, pinta uma área de 2m2.
+         
+Challenge: Make a program that reads the width and the height of a wall in meters, calculate its area and quantity
+           need to paint it, knowing that each liter of paint paints and area of 2m2.
+'''
