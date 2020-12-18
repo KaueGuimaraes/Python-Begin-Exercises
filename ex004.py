6b5ade39911beff7bd9f92ccb4884b3b5edcbded
KaueGuimaraes <76141331+KaueGuimaraes@users.noreply.github.com>
2020-12-17 21:20:37 -0500

    Update ex004.py

--- ex004.py
+++ ex004.py
@@ -6,6 +6,22 @@ print('\nÉ um número? ', letra.isnumeric())
 print('É alfabético? ', letra.isalpha())
 print('É alfanúmerico? ', letra.isalnum())
 print('Está em maiúsculo? ', letra.isupper())
-print('Está em minúsculo? ), letra.islower())
+print('Está em minúsculo? ', letra.islower())
 
 print('\nFIM!!')
+
+'''
+
+print('========= CHALLENGE 004 =========\n')
+
+letra = input('Type Something: ')
+
+print('\nIs numeric? ', letra.isnumeric())
+print('Is alpha? ', letra.isalpha())
+print('Is alpha numeric? ', letra.isalnum())
+print('Is upper? ', letra.isupper())
+print('Is lower? ', letra.islower())
+
+print('\nEND!!')
+
+'''
