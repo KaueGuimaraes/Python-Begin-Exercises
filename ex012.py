2575eae0200fba7f16c99fbb3a314b8fce96f567
KaueGuimaraes <76141331+KaueGuimaraes@users.noreply.github.com>
2020-12-22 15:05:50 -0500

    Add files via upload

---
+++ ex012.py
@@ -0,0 +1,29 @@
+print('========= DESAFIO 012 =========\n')
+
+p = int(input('Digite o preço de um produto: '))
+q = (p / 100) * 5
+
+print('\nO preço desse produto com 5% de de desconto será de R${} '.format(p-q))
+
+print('\nFIM!!')
+
+'''
+
+English Version
+
+print('========= CHALLENGE 012 =========\n')
+
+p = int(input('Enter a price of a product: '))
+q = (p / 100) * 5
+
+print('\nThe price of this product with 5% discount is ${} '.format(p-q))
+
+print('\nEND!!')
+
+'''
+
+'''
+Desafio: Faça um algoritimo que leia o preço de um produto e mostre seu novo prço com 5% de desconto.
+
+Challenge: Write a script that reads the price of a product and shows its new price with 5% discount.
+'''
